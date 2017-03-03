e9cd1b832563d115dd6541a1138cc7b1eb381eb8
Mike Gaughan <michael.t.gaughan@gmail.com>
2017-03-02 22:25:36 -0700

    Progress save

--- src/api/properties.js
+++ src/api/properties.js
@@ -3,9 +3,10 @@ import { Router } from 'express'
 export default ({ config, db }) => {
   let api = Router()
 
-  api.get('/', (req, res) => {
+  api.get('/properties', (req, res) => {
     db.collection('properties').find().toArray((err, result) => {
       if (err) throw err
+
       res.json(result)
     })
   })
--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import cors from 'cors'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
 import middleware from './middleware'
-import api from './api'
+import api from './api/properties'
 import config from './config.json'
 import mongodb from 'mongodb'
 
+const path = require('path')
 let app = express()
+
 app.server = http.createServer(app)
 
 // logger
@@ -23,13 +25,18 @@ app.use(bodyParser.json({
   limit: config.bodyLimit
 }))
 
+// app.use('/assets', express.static(`${__dirname}/assets`))
+
 // connect to db
-mongodb.MongoClient.connect('mongodb://localhost:27017/apitemplate', (err, db) => {
+mongodb.MongoClient.connect('mongodb://localhost:27017/crowdnine', (err, db) => {
   // internal middleware
   app.use(middleware({ config, db }))
 
+  // app.get('/', (req, res) => {
+  //   res.sendFile(`${__dirname}/index.html`)
+  // })
   // api router
-  app.use('/api', api({ config, db }))
+  app.use('/', api({ config, db }))
 
   app.server.listen(process.env.PORT || config.port)
 
